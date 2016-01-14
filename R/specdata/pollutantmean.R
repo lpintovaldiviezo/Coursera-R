cac157267681b0c4600c3735a165a01229ce515b
Luis Alejandro Pinto Valdiviezo <lpintovaldiviezo@gmail.com>
2016-01-14 18:57:44 -0500

    R delete

--- R/specdata/pollutantmean.R
+++
@@ -1,7 +0,0 @@
-pollutantmean <- function(directory, pollutant, id = 1:322){
-	for (i in id){
-		namefile <- paste(directory, id, sep="/")
-		namefile <- paste(namefile, .csv, sep="")
-		print (namefile)	
-	}
-}
