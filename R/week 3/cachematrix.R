e237b95f54df4479a75b145833def07ddc0ccf34
Luis Alejandro Pinto Valdiviezo <lpintovaldiviezo@gmail.com>
2016-01-18 16:39:13 -0500

    R

--- R/week 3/cachematrix.R
+++ R/week 3/cachematrix.R
@@ -1,7 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix is used to stored in memory the matrix which has been passed as parameter.
 
 makeCacheMatrix <- function(x = matrix()) {
 	  inverse <- NULL
@@ -17,7 +15,7 @@ makeCacheMatrix <- function(x = matrix()) {
              getinverse = getinverse)
 }
 
-
+## cacheSolve is used to returned the inverse of the matrix passed as parameter. It can return the value stored in memory if it exists or calculated it.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 	  inverse <- x$getinverse()
